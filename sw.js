de5ada105f09983bae2764862e2d43f43267d2c9
田中康之 <tanakayasuyuki@tanakayasuyukinoMacBook-Pro.local>
2020-08-16 14:09:59 +0900

    first commit

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install',function(event)){
+
+    console.log('ServiceWorker Install');
+});
+
+self.addEventListener('activate',function(event)){
+
+    console.log('ServiceWorker Activate');
+});
+
+self.addEventListener('fetch',function(event)){
+
+    console.log('ServiceWorker Fetch');
+});
